b64844595df63a10cdf7c47f35922efca9b27152
Lautaro Flores <lflores3@mail.austral.edu.ar>
2025-08-11 12:31:04 -0300

    Add files via upload

--- exercise_math.py
+++ exercise_math.py
@@ -1,3 +1,16 @@
 def math():
     a = 57
     b = 7
+    print(a+b)
+    print(a-b)
+    print(a*b)
+    abi=(a+b)/2
+    print(abi)
+    ab=a/b
+    abii=int(ab)
+    print(abii)
+    ac=a%b
+    aci=int(ac)
+    print(aci)
+    print(ab)
+#math()
